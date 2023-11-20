5216b9957ce12a341cf8f75ef3e5c47d21612171
matthew <mashh@live.co.uk>
2023-11-20 15:23:31 +0000

    refactored test to use toMatchObject

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -17,10 +17,19 @@ describe('GET /api/topics', () => {
         .get('/api/topics')
         .expect(200)
         .then(({ body }) => {
-            expect(Object.keys(body[0]))
-            .toEqual(expect.arrayContaining(
-                ['slug', 'description'])
-            )
+            expect(body).toMatchObject([
+                {
+                  description: 'The man, the Mitch, the legend',
+                  slug: 'mitch'
+                },
+                {
+                  description: 'Not dogs',
+                  slug: 'cats'
+                },
+                {
+                  description: 'what books are made of',
+                  slug: 'paper'
+                }])
         })
     });
 });
