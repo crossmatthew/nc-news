deb9f01ad25f310ba3ee41cca547780873ed3cf1
matthew <mashh@live.co.uk>
2023-11-20 15:29:03 +0000

    added data to be sent back as a value of topics inside an object

--- controllers/topics-controller.js
+++ controllers/topics-controller.js
@@ -3,7 +3,7 @@ const { allTopics } = require("../models/topics-model")
 exports.getTopics = (req, res, next) => {
     allTopics(req)
     .then((data) => {
-        res.status(200).send(data)
+        res.status(200).send({topics: data})
     })
     .catch((err) => {
         next(err)
