1d5b7ab7cd7f37bd47b540e31c0dcb7b93b3abaa
matthew <mashh@live.co.uk>
2023-11-20 13:50:27 +0000

    GET route for /api/topics, an error handler, and 404 handler

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const { getTopics } = require('./controllers/topics-controller');
+const { handleCustomErrors } = require('./controllers/errorhandlers')
 
 
 app.get('/api/topics', getTopics);
+
+app.use(handleCustomErrors);
+app.use((req, res, next) => {
+    res.status(404).send("Not Found!")
+  })
+
+module.exports = app;
