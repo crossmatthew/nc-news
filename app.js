f109d066836137a88719a0ffc923fd7d799fd62d
matthew <mashh@live.co.uk>
2023-11-20 16:09:50 +0000

    removed error handler

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const { getTopics } = require('./controllers/topics-controller');
-const { handleCustomErrors } = require('./controllers/errorhandlers')
 
 
 app.get('/api/topics', getTopics);
 
-app.use(handleCustomErrors);
 app.use((req, res, next) => {
     res.status(404).send("Not Found!")
   })
--- controllers/errorhandlers.js
+++
@@ -1,6 +0,0 @@
-exports.handleCustomErrors = (err, req, res, next) => {
-    if (err.status) {
-        res.status(err.status).send({message: err.msg})
-    }
-    next(err)
-}
