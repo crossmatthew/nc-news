3cf4d3e50fb810ddc55e1c5bd45a5ee3c13e8b0d
matthew <mashh@live.co.uk>
2023-11-21 14:34:59 +0000

    extracted data from its array

--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -3,7 +3,7 @@ const { specificArticles } = require("../models/articles-model")
 exports.getArticle = (req, res, next) => {
     specificArticles(req)
     .then((data) => {
-        res.status(200).send({article: data})
+        res.status(200).send({article: data[0]})
     })
     .catch((err) => {
         next(err)
