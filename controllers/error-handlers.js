9c62e255da56785509010ed1043f840cc34632d1
matthew <mashh@live.co.uk>
2023-11-21 14:02:13 +0000

    added a handler for 404

--- controllers/error-handlers.js
+++ controllers/error-handlers.js
@@ -1,6 +1,3 @@
-exports.handleCustomErrors = (err, req, res, next) => {
-    if (err.status) {
-        res.status(err.status).send({message: err.msg})
-    }
-    next(err)
+exports.handleFourZeroFour = (req, res, next) => {
+    res.status(404).send('Not Found!')
 }
