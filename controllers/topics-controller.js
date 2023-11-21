9582b717f2047c33f52b8c3e6995de6059f1e438
matthew <mashh@live.co.uk>
2023-11-21 11:25:50 +0000

    added controller for /api path

--- controllers/topics-controller.js
+++ controllers/topics-controller.js
@@ -1,9 +1,18 @@
-const { allTopics } = require("../models/topics-model")
+const { allTopics, allEndpoints } = require("../models/topics-model")
 
 exports.getTopics = (req, res, next) => {
     allTopics(req)
     .then((data) => {
         res.status(200).send({topics: data})
+    })
+    .catch((err) => {
+        next(err)
+    })
+}
+exports.getEndpointsJson = (req, res, next) => {
+    allEndpoints(req)
+    .then((data) => {
+        res.status(200).send({endpoints: data})
     })
     .catch((err) => {
         next(err)
