7d2a8fd4938d03a907a3ee91326ed1f57a1dfe5f
matthew <mashh@live.co.uk>
2023-11-21 12:13:59 +0000

    handle request for articles by article_id

--- controllers/topics-controller.js
+++ controllers/topics-controller.js
@@ -1,4 +1,4 @@
-const { allTopics, allEndpoints } = require("../models/topics-model")
+const { allTopics, allEndpoints, specificArticles } = require("../models/topics-model")
 
 exports.getTopics = (req, res, next) => {
     allTopics(req)
@@ -13,6 +13,15 @@ exports.getEndpointsJson = (req, res, next) => {
     allEndpoints(req)
     .then((data) => {
         res.status(200).send({endpoints: data})
+    })
+    .catch((err) => {
+        next(err)
+    })
+}
+exports.getArticle = (req, res, next) => {
+    specificArticles(req)
+    .then((data) => {
+        res.status(200).send({article: data})
     })
     .catch((err) => {
         next(err)
