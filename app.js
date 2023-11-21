0299e5079e480656cc2e42efa7cef8564b5d878f
matthew <mashh@live.co.uk>
2023-11-21 12:14:56 +0000

    .get path for articles via article_id

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const { getEndpointsJson, getTopics, getArticle } = require('./controllers/topics-controller');
+
 app.use(express.json());
-const { getTopics, getEndpointsJson } = require('./controllers/topics-controller');
 
-
-app.get('/api/topics', getTopics);
 app.get('/api', getEndpointsJson);
+app.get('/api/topics', getTopics);
+app.get('/api/articles/:article_id', getArticle);
 
 app.use((req, res, next) => {
     res.status(404).send("Not Found!")
