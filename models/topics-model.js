928afa96088e27a44319da19b03ed8ae2c2a6236
matthew <mashh@live.co.uk>
2023-11-21 11:26:25 +0000

    model for /api path which reads endpoints.json file

--- models/topics-model.js
+++ models/topics-model.js
@@ -1,8 +1,18 @@
-const db = require('../db/connection')
+const db = require('../db/connection');
+const fs = require('fs/promises');
 
 exports.allTopics = (req) => {
     return db.query(`SELECT * FROM topics;`)
     .then((result) => {
         return result.rows;
+    })
+};
+exports.allEndpoints = (req) => {
+    return fs.readFile('./endpoints.json', 'utf-8', (err, data) => {
+        if (err) next(err)
+        return data
+    })
+    .then((data) => {
+        return JSON.parse(data)
     })
 }
