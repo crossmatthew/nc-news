8f814a941c94353901ef291573ad9b7578b64521
matthew <mashh@live.co.uk>
2023-11-21 15:04:59 +0000

    function to handle PSQL errors

--- controllers/error-handlers.js
+++ controllers/error-handlers.js
@@ -1,3 +1,9 @@
 exports.handleFourZeroFour = (req, res, next) => {
     res.status(404).send('Not Found!')
 }
+exports.handlePsqlErrors = (err, req, res, next) => {
+	if (err.code.startsWith('22')) {
+		res.status(400).send({ message: 'Bad Request' });
+	}
+	next(err)
+};
