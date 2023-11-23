a7ed2920f303075b27b10962c34c1972bb5916bd
matthew <mashh@live.co.uk>
2023-11-23 12:30:39 +0000

    moved module.exports to bottom of file

--- utils/checkExists.js
+++ utils/checkExists.js
@@ -1,16 +1,14 @@
 const db = require('../db/connection');
 const format = require('pg-format');
-
-module.exports = checkExists
-
 function checkExists(table, column, value) {
     const queryStr = format(`SELECT * FROM %I WHERE %I = $1`, table, column)
     return db.query(queryStr, [value])
     .then((result) => {
         if (result.rows.length === 0) {
             return Promise.reject()
-            } else {
-                return result.rows
-            }
-        })
-    }
+        } else {
+            return result.rows
+        }
+    })
+};
+module.exports = checkExists
