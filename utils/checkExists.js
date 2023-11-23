9a9e4e2ed5ff36dcc248674567b3839687b6c98e
matthew <mashh@live.co.uk>
2023-11-23 12:03:21 +0000

    removed require for specific error-handler, return result.rows rather than results

--- utils/checkExists.js
+++ utils/checkExists.js
@@ -1,4 +1,3 @@
-const { handleFourZeroFour } = require('../controllers/error-handlers');
 const db = require('../db/connection');
 const format = require('pg-format');
 
@@ -6,10 +5,10 @@ module.exports = checkExists
 
 function checkExists(table, column, value) {
     const queryStr = format(`SELECT * FROM %I WHERE %I = $1`, table, column)
-    const dbOutput = db.query(queryStr, [value])
+    return db.query(queryStr, [value])
     .then((result) => {
         if (result.rows.length === 0) {
-            return Promise.reject({ status: 404, message: 'Not Found!' })
+            return Promise.reject()
             } else {
                 return result.rows
             }
