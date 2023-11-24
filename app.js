23c5e6874d6e3b8d0428db4abb9b480d33aef274
matthew <mashh@live.co.uk>
2023-11-24 12:20:15 +0000

    path for article path, app.patch

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getEndpointsJson } = require('./controllers/endpoints-controller');
-const { getArticle, getAllArticles } = require('./controllers/articles-controller');
+const { getArticle, getAllArticles, patchArticle } = require('./controllers/articles-controller');
 const { getTopics } = require('./controllers/topics-controller');
 const { handleFourZeroFour, handlePsqlErrors, handleCustomErrors, handleFiveHundred } = require('./controllers/error-handlers');
 const { getComments, postComment } = require('./controllers/comments-controller');
@@ -15,6 +15,7 @@ app.get('/api/articles', getAllArticles);
 app.get('/api/articles/:article_id', getArticle);
 app.get('/api/articles/:article_id/comments', getComments);
 app.post('/api/articles/:article_id/comments', postComment);
+app.patch('/api/articles/:article_id', patchArticle);
 
 app.all('*', handleFourZeroFour);
 app.use(handlePsqlErrors);
