9585fc2050c39351166beec41cadbcefdc8f8b94
matthew <mashh@live.co.uk>
2023-11-24 13:31:28 +0000

    tests for delete comments path

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -209,3 +209,26 @@ describe('POST /api/articles/:article_id/comments', () => {
         })
     });
 });
+describe.skip('DELETE /api/comments/:comment_id', () => {
+    test('should return a status code of 204 after a comment has been deleted by comment_id', () => {
+        return request(app)
+        .delete('/api/comments/3')
+        .expect(204)
+    });
+    test('should return 404 status code if trying to delete a non-existent comment', () => {
+        return request(app)
+        .delete('/api/comments/333000333')
+        .expect(404)
+        .then(({ body }) => {
+            expect(body.msg).toBe('Not Found!')
+        })  
+    });
+    test('should return 400 status code if trying to delete a comment without passing comment_id as a number', () => {
+        return request(app)
+        .delete('/api/comments/comment')
+        .expect(400)
+        .then(({ body }) => {
+            expect(body.msg).toBe('Bad Request')
+        })
+    });
+});
