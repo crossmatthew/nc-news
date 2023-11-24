0cd11cf207594cb60dd466a2e03f44284de7d38a
matthew <mashh@live.co.uk>
2023-11-24 18:02:16 +0000

    every specific article will now total the number of comments for this article

--- models/articles-model.js
+++ models/articles-model.js
@@ -3,18 +3,19 @@ const checkExists = require('../utils/checkExists');
 
 exports.specificArticle = (req) => {
     const { params } = req
-    if (typeof (params.article_id * 1) !== 'number') {
-        return Promise.reject()
-    }
-    return db.query(`
-    SELECT * FROM articles
-    WHERE article_id = $1;`, [params.article_id])
+    return checkExists('articles', 'article_id', params.article_id)
+    .then(() => {
+        return db.query(`
+        SELECT articles.*, COUNT(comments.comment_id) AS comment_count
+        FROM articles LEFT JOIN comments
+        ON articles.article_id = comments.article_id
+        WHERE articles.article_id = $1
+        GROUP BY articles.article_id
+        ORDER BY created_at DESC;
+        `, [params.article_id])
+    })
     .then((data) => {
-        if (!data.rows.length) {
-            return Promise.reject()
-        } else {
         return data.rows
-        }
     })
 };
 exports.allArticles = (req) => {
