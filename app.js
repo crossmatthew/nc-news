4c311ab87e226740b11d4e01b16d087ea37f90cc
matthew <mashh@live.co.uk>
2023-11-26 18:55:08 +0000

    removed existing app.js code to use router instead

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
 const express = require('express');
-const { getEndpointsJson } = require('./controllers/endpoints-controller');
-const { getArticle, getAllArticles, patchArticle } = require('./controllers/articles-controller');
-const { getTopics } = require('./controllers/topics-controller');
 const { handleFourZeroFour, handlePsqlErrors, handleCustomErrors, handleFiveHundred } = require('./controllers/error-handlers');
-const { getComments, postComment, deleteComment } = require('./controllers/comments-controller');
-const { getUsers } = require('./controllers/users-controller');
+const apiRouter = require('./routes/api-router');
 const app = express();
 
-
 app.use(express.json());
-
-app.get('/api', getEndpointsJson);
-app.get('/api/topics', getTopics);
-app.get('/api/users', getUsers);
-app.get('/api/articles', getAllArticles);
-app.get('/api/articles/:article_id', getArticle);
-app.get('/api/articles/:article_id/comments', getComments);
-app.post('/api/articles/:article_id/comments', postComment);
-app.delete('/api/comments/:comment_id', deleteComment);
-app.patch('/api/articles/:article_id', patchArticle);
-
+app.use('/api', apiRouter)
 app.all('*', handleFourZeroFour);
 app.use(handlePsqlErrors);
 app.use(handleCustomErrors);
 app.use(handleFiveHundred);
-
   
 module.exports = app;
