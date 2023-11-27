8eb43e3633b66722e4ba213438078e7e20efc319
matthew <mashh@live.co.uk>
2023-11-27 17:26:50 +0000

    added model for getting a user by username

--- models/users-model.js
+++ models/users-model.js
@@ -1,8 +1,23 @@
 const db = require('../db/connection');
+const checkValueExists = require('../utils/checkValueExists');
 
 exports.allUsers = (req) => {
     return db.query(`SELECT * FROM users;`)
     .then((result) => {
         return result.rows;
+    })
+};
+exports.specificUser = (req) => {
+    const { params } = req
+    console.log(params.username)
+    return checkValueExists('users', 'username', params.username)
+    .then(() => {
+        return db.query(`
+        SELECT * FROM users
+        WHERE username = $1
+        `, [params.username])
+    })
+    .then((data) => {
+        return data.rows
     })
 };
