b00a9b879e9a35b6af8286a2405b4325311b9e70
matthew <mashh@live.co.uk>
2023-11-30 17:53:24 +0000

    updated topics model for POST method

--- models/topics-model.js
+++ models/topics-model.js
@@ -6,3 +6,13 @@ exports.allTopics = (req) => {
         return result.rows;
     })
 };
+exports.topicToPost = (req) => {
+    const { body } = req
+        return db.query(`
+        INSERT INTO topics (description, slug)
+        VALUES ($1, $2)
+        RETURNING *`, [body.description, body.slug])
+        .then((data) => {
+            return { topic: data.rows[0] }
+    })
+};
