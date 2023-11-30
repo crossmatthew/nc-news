5e17208cb3340beebf9d5d50383f84523e814f3e
matthew <mashh@live.co.uk>
2023-11-30 17:53:42 +0000

    updated topics controller for POST method

--- controllers/topics-controller.js
+++ controllers/topics-controller.js
@@ -1,4 +1,4 @@
-const { allTopics } = require("../models/topics-model")
+const { allTopics, topicToPost } = require("../models/topics-model");
 
 exports.getTopics = (req, res, next) => {
     allTopics(req)
@@ -8,4 +8,13 @@ exports.getTopics = (req, res, next) => {
     .catch((err) => {
         next(err)
     })
-}
+};
+exports.postTopic = (req, res, next) => {
+    topicToPost(req)
+    .then((data) => {
+        res.status(201).send(data)
+    })
+    .catch((err) => {
+        next(err)
+    })
+};
