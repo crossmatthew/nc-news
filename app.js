a400feacc4d926ee0ddf727b70f62d2fc839570f
matthew <mashh@live.co.uk>
2023-12-11 10:08:41 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const cors = require('cors');
 const express = require('express');
 const { handleFourZeroFour, handlePsqlErrors, handleCustomErrors, handleFiveHundred } = require('./controllers/error-handlers');
 const apiRouter = require('./routes/api-router');
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use('/api', apiRouter)
 app.all('*', handleFourZeroFour);
